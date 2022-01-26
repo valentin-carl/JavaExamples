eb5fccd4b29424c4759789abdb05dc527aef326b
valentin-carl <87319498+valentin-carl@users.noreply.github.com>
2022-01-26 18:11:29 +0100

    Update AdderClient.java

--- ClientServerExample/src/AdderClient.java
+++ ClientServerExample/src/AdderClient.java
@@ -1,15 +1,54 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class AdderClient extends Thread {
- 
-  String hostName;
-  int portNumber;
-  
-  public AdderClient (String hostName, int portNumber) {
-    this.hostName = hostName;
-    this.portNumber = portNumber;
-  }  
-  
-  @Override
-  public void run () {
-     
-  }
+
+    static int nextID = 0;
+    int id = nextID++;
+    String hostName;
+    int portNumber;
+
+    public AdderClient (String hostName, int portNumber) {
+        this.hostName = hostName;
+        this.portNumber = portNumber;
+    }
+
+    @Override
+    public String toString () {
+        return "adderClient" + this.id;
+    }
+
+    public void run () {
+
+        // get inputs
+        System.out.println(this + " is asking for a number");
+        int num = new Scanner(System.in).nextInt();
+
+        try (
+                // connect to server
+                Socket socket = new Socket(this.hostName, this.portNumber);
+                OutputStream output = socket.getOutputStream();
+                InputStream inputStream = socket.getInputStream();
+                ) {
+            // confirm connection
+            System.out.println(this + " connected to " + socket);
+
+            // send number
+            output.write(num);
+            System.out.println(this + " sent " + num);
+
+            // wait for input
+            int sum = inputStream.read();
+            System.out.println(this + " got " + sum + " as result.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // signals end of client thread
+        System.out.println(this + " finished");
+    }
 }
