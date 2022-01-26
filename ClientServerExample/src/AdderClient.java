a2ace2f1aad6b1e2994f3fa0ecdbf11b0e1139d4
valentin-carl <87319498+valentin-carl@users.noreply.github.com>
2022-01-26 14:49:55 +0100

    Create AdderClient.java

---
+++ ClientServerExample/src/AdderClient.java
@@ -0,0 +1,15 @@
+public class AdderClient extends Thread {
+ 
+  String hostName;
+  int portNumber;
+  
+  public AdderClient (String hostName, int portNumber) {
+    this.hostName = hostName;
+    this.portNumber = portNumber;
+  }  
+  
+  @Override
+  public void run () {
+     
+  }
+}
